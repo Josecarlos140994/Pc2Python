74c4906bae5bee02a9c0172c212eed48db31b000
Josecarlos140994 <162035043+Josecarlos140994@users.noreply.github.com>
2024-03-16 19:22:37 +0000

    solucion 10

--- pregunta10.py
+++ pregunta10.py
@@ -1,9 +1,11 @@
+
+
 def convertir_fecha(fecha_input):
     meses = [
         "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
         "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
     ]
- 
+  
     try:
         partes = fecha_input.split('/')
         if len(partes) == 3:
@@ -12,14 +14,12 @@ def convertir_fecha(fecha_input):
     except ValueError:
         pass
     
-
     try:
         mes_texto, dia, año = fecha_input.replace(',', '').split()
         mes = str(meses.index(mes_texto) + 1).zfill(2)
         return f"{año}-{mes}-{dia.zfill(2)}"
     except ValueError:
         return "Formato de fecha no reconocido."
-
 
 fecha_input = input("Ingrese una fecha: ")
 fecha_output = convertir_fecha(fecha_input)
