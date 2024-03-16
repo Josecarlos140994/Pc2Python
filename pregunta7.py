d2571a1d616aea958f8c134ada74c44cffdf80fe
Josecarlos140994 <162035043+Josecarlos140994@users.noreply.github.com>
2024-03-16 16:34:47 +0000

    solucion 7

---
+++ pregunta7.py
@@ -0,0 +1,19 @@
+##Escriba una función de Python que tome un número como parámetro y verifique que el número sea
+##primo o no.
+
+def es_primo(numero):
+
+    if numero <= 1:
+        return False
+    
+    for i in range(2, numero):
+        if numero % i == 0:
+            return False
+    return True
+
+numero = int(input("Ingrese un número para verificar si es primo: "))
+
+if es_primo(numero):
+    print(f"El número {numero} es primo.")
+else:
+    print(f"El número {numero} no es primo.")
